5497df9ab30595f460db950500ab3ade6280ff09
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-04-19 14:18:52 +0200

    add first signup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
+
+import Nav from "../../components/nav/Nav";
 
 const Home = () => {
-    return (
-        <div>Home</div>
-    );
-}
+
+  const authToken = false;
+
+  const handleClick = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <>
+      <Nav />
+      <div className="home">
+        <h1>Swipe Right</h1>
+        <button className="primary-button" onClick={handleClick}>
+          {authToken ? "Signout" : "Create Account"}
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Home;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -2,7 +2,64 @@ import React from 'react';
 
 const Signup = () => {
     return (
-        <div>SignUp</div>
+        <>Crear una cuenta
+            <form action="/signup" method="">
+                <label for="firstName">
+                    <p>Nombre</p>
+                    <input name="firstName" type="text"/>
+                </label>
+
+                <label for="lastName">
+                    <p>Apellidos</p>
+                    <input name="lastName" type="text"/>
+                </label>
+
+                <label for="birthday">
+                    <p>Cumpleaños</p>
+                    <input name="day" type="number"/> 
+                    <input name="month" type="number"/> 
+                    <input name="year" type="number"/> 
+                </label>  
+
+                <label for="gender">
+                    <p>Gender</p>
+                </label>
+
+                <label for="animal">
+                    <p>Animal</p>
+                </label>
+
+                <label for="showme">
+                    <p>Interesado en</p>
+                </label>
+
+                <label for="aboutme">
+                    <p>Sobre mí</p>
+                    <textarea name="aboutme" type="text"/>
+                </label>
+
+                <label for="email">
+                    <p>Email</p>
+                    <input name="email" type="text"/>
+                </label>
+
+                <label for="password">
+                    <p>Contraseña</p>
+                    <input name="password" type="password"/>
+                </label>
+
+                <label for="password">
+                    <p>Confirmar contraseña</p>
+                    <input name="password" type="password"/>
+                </label>
+
+                <input type="submit" value="Enviar"/>
+
+
+
+            </form>
+        
+        </>
     );
 }
 
