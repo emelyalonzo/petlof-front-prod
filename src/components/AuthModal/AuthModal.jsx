305d9eba74eb494be129a0688da7adf7c719a035
Emely Alonzo <emelyalonzom@gmail.com>
2022-05-19 17:38:43 +0200

    Made styling and ternary functionality on authmodalUI

--- src/components/AuthModal/AuthModal.jsx
+++ src/components/AuthModal/AuthModal.jsx
@@ -1,9 +1,71 @@
 import React from 'react';
+import {useState} from "react";
 
-const Authmodal = () => {
+
+const Authmodal = ({setShowModal, isSignUp }) => {
+
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [confirmPassword, setConfirmPassword] = useState(null);
+    const [error, setError] = useState(null);
+
+    const handleClick = () => {
+        setShowModal(false);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        try {
+            if (isSignUp && ( password !== confirmPassword)) {
+                setError('Passwords does not match')
+            }
+            // TODO POST REQUEST TO OUR DB
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
     return (
-        <div>
-            
+        <div className="auth-modal">
+            <div className="close-icon" onClick={handleClick}>
+            ⨂
+            </div>
+            <h2>{isSignUp ? "CREATE ACCOUNT" : "LOG IN"}</h2>
+            <p>By clicking login, you agree to our terms. Learn how we process your data in our Privacy Policy and Cookie Policy</p>
+        
+            <form onSubmit={handleSubmit}>
+                <input 
+                type="email"
+                name="email" 
+                id="email" 
+                placeholder="Email address" 
+                required={true} 
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <input 
+                type="password" 
+                name="password" 
+                id="password" 
+                placeholder="Password" 
+                required={true} 
+                onChange={(e) => setPassword(e.target.value)}
+                />
+                {isSignUp && <input 
+                type="password" 
+                name="passwordCheck" 
+                id="passwordCheck" 
+                placeholder="Repeat your password" 
+                required={true} 
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                />}
+                <input className="secondary-button" type="submit"/>
+                <p>{error}</p>
+
+            </form>
+            <hr/>
+            <h2>GET THE APP</h2>
         </div>
     );
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const Nav = () => {
+const Nav = ({minimal, authToken, setShowModal, showModal, setIsSignUp}) => {
+
+    const handleClick = () => {
+        setShowModal(true);
+        setIsSignUp(false); //its false because when clicking we are logging in
+    }
+
     return (
-        <div>
-            
-        </div>
+        <nav>
+            {/* Homepage UI TODO */}
+
+            {!authToken && !minimal && <button
+                className="nav-button"
+                onClick={handleClick}
+                disabled={showModal}>
+            Log in</button>}
+        </nav>
     );
 }
 
